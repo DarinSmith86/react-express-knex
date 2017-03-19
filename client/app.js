32612a7c1b54819c8199efe4082cebd086fdf00d
darin smith <darinsmith86@gmail.com>
2017-03-19 16:31:00 -0600

    MVP

---
+++ client/app.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { Router, browserHistory } from 'react-router';
+
+// import './sass/style.scss';
+// import reducers from './reducers';
+// import routes from './routes';
+// import store from './store';
+
+ReactDOM.render(
+  <h1>Hello World</h1>
+  , document.querySelector('#main'));
+
+  //html
+  //jsx html in javscript
+  //
+  // module.exports
+  // require() //server
+
+  // import/export //client
+
+  // import axios from 'axios';
+  // 
+  // axios.post('http://localhost:3000/', {data: 123})
+  // .then((data) => {
+  //   console.log('data', data);
+  // })
+  // .catch((err) => {
+  //   console.log('err', err);
+  // });
---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var cors = require('cors');
+
+var routes = require('./server/routes/routes.js');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/', routes);
+
+app.all('*', function(req, res, next) {
+    res.sendFile('index.html', {
+        root: __dirname + '/dist/'
+    });
+});
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
